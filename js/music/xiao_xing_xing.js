639a4eda5826afec67659d551669ca3a5f323eb5
bugmaker <289115128@qq.com>
2021-03-03 17:54:39 +0800

    初始版本

---
+++ js/music/xiao_xing_xing.js
@@ -0,0 +1,106 @@
+var yinfu = {
+    beatInSection: 4,
+    noteInBeat: 4,
+    rightNotes: [
+        {
+            section: 1,
+            note: [
+                {
+                    startBeat: 0,
+                    length: 4,
+                    area: 5,
+                    musicalAlphabet: 1
+                },
+                {
+                    startBeat: 1,
+                    length: 4,
+                    area: 5,
+                    musicalAlphabet: 1
+                },
+                {
+                    startBeat: 2,
+                    length: 4,
+                    area: 5,
+                    musicalAlphabet: 5
+                },
+                {
+                    startBeat: 3,
+                    length: 4,
+                    area: 5,
+                    musicalAlphabet: 5
+                }
+            ]
+        },
+        {
+            section: 2,
+            note: [
+                {
+                    startBeat: 0,
+                    length: 4,
+                    area: 5,
+                    musicalAlphabet: 6
+                },
+                {
+                    startBeat: 1,
+                    length: 4,
+                    area: 5,
+                    musicalAlphabet: 6
+                },
+                {
+                    startBeat: 2,
+                    length: 2,
+                    area: 5,
+                    musicalAlphabet: 5
+                }
+            ]
+        }
+    ],
+    leftNotes: [
+        {
+            section: 1,
+            note: [
+                {
+                    startBeat: 0,
+                    length: 1,
+                    area: 3,
+                    musicalAlphabet: 1
+                },
+                {
+                    startBeat: 0,
+                    length: 1,
+                    area: 3,
+                    musicalAlphabet: 3
+                }
+            ]
+        },
+        {
+            section: 2,
+            note: [
+                {
+                    startBeat: 0,
+                    length: 2,
+                    area: 3,
+                    musicalAlphabet: 1
+                },
+                {
+                    startBeat: 0,
+                    length: 2,
+                    area: 3,
+                    musicalAlphabet: 4
+                },
+                {
+                    startBeat: 2,
+                    length: 2,
+                    area: 3,
+                    musicalAlphabet: 1
+                },
+                {
+                    startBeat: 2,
+                    length: 2,
+                    area: 3,
+                    musicalAlphabet: 3
+                }
+            ]
+        }
+    ]
+};
